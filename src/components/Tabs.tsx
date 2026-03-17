fef65d2ad3ee884c06bfad684c6ac8e2c9724d1f
ThomasHSON <hson_thomas@kutech.tw>
2026-03-17 16:26:25 +0800

    Updated Tabs.tsx

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -18,7 +18,7 @@ const Tabs = () => {
             {t('tabs.review')}
           </a>
           <a href="../inventory_daily_report" className="px-4 py-2 text-base font-medium border-b-2 border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300">
-            {t('tabs.review')}
+            {t('tabs.daily')}
           </a>
         </div>
       </div>
